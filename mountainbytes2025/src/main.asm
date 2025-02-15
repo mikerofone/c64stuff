4af325b2fa3e5bbdfba9ae23bdd69bf40f7a27b3
Nikolai Knopp <mike_rofone@imail.de>
2025-02-15 20:44:38 +0100

    Fixed some bugs with color updating and some botched additions (forgot to clear carry). Still have some bugs with wrapping the cursor around.

--- mountainbytes2025/src/main.asm
+++ mountainbytes2025/src/main.asm
@@ -15,11 +15,12 @@ BasicUpstart2(main)
         .return next_zp++ - 1
 }
 // zpw: Word (address), zpb: Byte
+.const zpw_cursoraddr = res_zpw()       // Global: Position of the cursor (0-999).
 .const zpw_textaddr = res_zpw()         // Param: Address of text to display
 .const zpb_targetrow = res_zpb()        // Param: Screen row to start printing at (0-24)
 .const zpb_targetcol = res_zpb()        // Param: Screen column to start printing at (0-39) 
-.const zpw_screenaddr = res_zpw()       // Local: Screen RAM address computed from (targetrow, targetcol).
-.const zpw_coloraddr = res_zpw()        // Local: Color RAM address computed from (targetrow, targetcol).
+.const zpw_screenaddr = res_zpw()       // Local: Screen RAM address computed from cursor.
+.const zpw_coloraddr = res_zpw()        // Local: Color RAM address computed from cursor.
 .const zpb_color = res_zpb()            // Param: Color to use for writing text.
 .const zpb_tempval = res_zpb()          // Local: Temporary byte that might get clobbered by any
                                         //        jump to subroutine.
@@ -27,48 +28,34 @@ BasicUpstart2(main)
         *=$4000 "Code"
 
 main:
-        // Init row and col.
-        lda #2
-        sta zpb_targetrow
-        pha                             // Push to stack so it can be recovered later.
-        lda #7
-        sta zpb_targetcol
-        pha                             // Push to stack so it can be recovered later.
         // Init color.
         lda #00
-        sta border_color_addr           // Border to black.
+        // sta border_color_addr           // Border to black.
         sta border_color_addr+1         // Background to black.
-        lda #01                         // Don't use black for forground.
+        lda #01                         // Don't use black for foreground.
         sta zpb_color
         jsr clearscreen
 flicker:
-
-        // Recover last values from stack, first col, then row.
-        pla
-        sta zpb_targetcol
-        pla
-        sta zpb_targetrow
         lda #<txtmike
         sta zpw_textaddr
         lda #>txtmike
         sta zpw_textaddr+1
         jsr printtext
-        jsr update_coords       // Or jsr down_one_line?
+        // Move cursor right by # chars printed.
+        jsr advance_cursor
+        //jsr update_coords
+        //jsr down_one_line?
 
         lda #<txtmountain
         sta zpw_textaddr
         lda #>txtmountain
         sta zpw_textaddr+1
         jsr printtext
-        jsr update_coords
+        // Move cursor right by # chars printed.
+        jsr advance_cursor
+        // jsr update_coords
         // ldx #100
         // jsr wait
-
-        // Save coords to stack so zpb_target{row,col} can be clobbered.
-        lda zpb_targetrow
-        pha
-        lda zpb_targetcol
-        pha
 
         jsr cycle_color
 
@@ -78,7 +65,7 @@ flicker:
 cycle_color:
         lda zpb_color
         ldx #01
-        ldy #16
+        ldy #15
         jsr add_and_clamp
         sta zpb_color
         bne !+                          // If zero (black), increment.
@@ -89,27 +76,72 @@ cycle_color:
 // Gets row and col from zpb_targetrow/zpb_targetcol, increments them
 // by some value and clamping to range, and writes them back.
 update_coords:
-        lda zpb_targetrow
-        ldx #03
-        ldy #25
-        jsr add_and_clamp
-        sta zpb_targetrow
+        // lda zpb_targetrow
+        // ldx #03
+        // ldy #25
+        // jsr add_and_clamp
+        // sta zpb_targetrow
         lda zpb_targetcol
         ldx #07
-        ldx #40                 // Bug: should be ldy, but results in
+        ldy #40                 // Bug: should be ldy, but results in
                                 // more interesting output. ¯\_(ツ)_/¯
         jsr add_and_clamp
         sta zpb_targetcol
+        jsr set_cursor_xy       // Update zpw_cursoraddr from zpb_targetcol/row.
         rts
 
 
-// Move cursor down one line, wrapping around the screen.
+// Move the cursor to zpb_targetrow and zpb_targetcol.
+// Clobbers A, X, Y.
+set_cursor_xy:
+        lda #00                 // Reset cursor to 0.
+        sta zpw_cursoraddr
+        sta zpw_cursoraddr+1
+        ldx zpb_targetrow       // Load rows to add.
+add_rows:
+        cpx #00                 // Test if more rows.
+        beq add_cols            // If none, skip.
+        dex
+        lda #40                 // Advance cursor by one row of chars.
+        jsr advance_cursor
+        jmp add_rows
+add_cols:
+        lda zpb_targetcol       // Advance cursor by column-many chars.
+        jsr advance_cursor
+        rts
+
+// Move cursor down one line, wrapping around the screen. Clobbers A, Y.
 down_one_line:
-        lda zpb_targetrow
-        ldx #01
-        ldy #25
-        jsr add_and_clamp
-        sta zpb_targetrow
+        lda #40
+        // Continue into advance_cursor.
+// Moves cursor right by the amount of characters in A. Clobbers A, Y.
+// Wraps around the screen.
+advance_cursor:
+        clc                     // Prepare addition.
+        adc zpw_cursoraddr      // Add cursor lo byte to # chars in A.
+        sta zpw_cursoraddr      // Update cursor lo byte.
+        bcc !return+            // If no overflow, no need to increment hi byte.
+        ldy zpw_cursoraddr+1    // Load cursor hi byte, increment and write back.
+        iny
+        sty zpw_cursoraddr+1
+        // If zpw_cursoraddr > $03E8, then wrap it around.
+        cpy #$03                // Compare hi byte.
+        bcc !return+            // Carry not set -> hi byte is < $02.
+        bne must_wrap           // Carry set, zero unset set -> hi byte is > $03
+        // Hi byte is #03, must compare lo byte: No overflow if that's <$E8.
+        lda zpw_cursoraddr      // Load lo byte to compare
+        cmp #$E8
+        bcc !return+            // Carry not set -> lo byte <$E8.
+must_wrap:
+        // Subtract $03E8.
+        sec                     // Set carry for subtraction
+        lda zpw_cursoraddr      // Start on lo byte.
+        sbc #$E8
+        sta zpw_cursoraddr      // Store new lo byte.
+        lda zpw_cursoraddr+1
+        sbc #$03
+        sta zpw_cursoraddr+1    // Store new lo byte. Should now be wrapped back.
+!return:
         rts
 
 
@@ -128,29 +160,27 @@ add_and_clamp:
 !return:
         rts
 
-// Fills the screen with spaces. Clobbers zpb_targetcol/row and zpb_textaddr.
+// Fills the screen with spaces. Clobbers zpb_textaddr and resets cursor address to 0.
 clearscreen:
         lda #<txtemptyblock
         sta zpw_textaddr
         lda #>txtemptyblock
         sta zpw_textaddr+1
-        // Step through rows in 5-increments, and paste a 5-row string of spaces.
+        // Step through rows from the top, pasting a 5-row string of spaces.
         lda #00
-        sta zpb_targetcol
-        sta zpb_targetrow               // Start at row 0.
+        sta zpw_cursoraddr
+        sta zpw_cursoraddr+1
         jsr printtext
-        lda #05
-        sta zpb_targetrow
+        jsr advance_cursor
         jsr printtext
-        lda #10
-        sta zpb_targetrow
+        jsr advance_cursor
         jsr printtext
-        lda #15
-        sta zpb_targetrow
+        jsr advance_cursor
         jsr printtext
-        lda #20
-        sta zpb_targetrow
-        jsr printtext
+        // Set cursor to top-left.
+        lda #00
+        sta zpw_cursoraddr
+        sta zpw_cursoraddr+1
         rts
 
 // Waits for a while, determined by value in X.
@@ -168,50 +198,28 @@ wait:
 !end:
         rts
 
-// Print the text starting at address in zpw_textaddr to the screen at coordinates
-// zpb_targetrow and zpb_targetcol.
+
+// Print the text at the current cursor position. See set_cursor_xy to set the cursor
+// to a specific position.
+// Returns number of characters printed in accumulator on return.
 printtext:
         // Assemble addresses for screen and color. Only hi byte differs, lo byte
         // can be shared for both screen and color.
         lda #>screen_base       // Init zpw_screenaddr.
+        clc
+        adc zpw_cursoraddr + 1  // Add cursor hi byte.
         sta zpw_screenaddr + 1
         lda #>color_base        // Init zpw_coloraddr.
+        clc
+        adc zpw_cursoraddr + 1
         sta zpw_coloraddr + 1
-        lda #<screen_base       // Low bytes for screen and color must be the same!
-        sta zpw_screenaddr
-        sta zpw_coloraddr
-        ldx zpb_targetrow       // Load remaining rows.
-add_rows:
-        cpx #00                 // Test if more rows.
-        beq rows_done           // If none, skip.
-        clc                     // Prepare addition.
-        adc #40                 // Add a line's worth of chars.
-        sta zpw_screenaddr      // Update screen and color address lo bytes.
-        sta zpw_coloraddr 
-        dex                     // Substract row.
-        bcc add_rows            // If no overflow, no need to increment hi byte.
-        ldy zpw_screenaddr+1    // Load screen/color hi bytes, increment and write back.
-        iny
-        sty zpw_screenaddr+1
-        ldy zpw_coloraddr+1
-        iny
-        sty zpw_coloraddr+1
-        jmp add_rows
-rows_done:
-        lda zpw_screenaddr      // Ensure lo byte is in A.
-        clc                     // Prepare addition.
-        adc zpb_targetcol       // Add column chars.
-        sta zpw_screenaddr      // Update lo bytes for screen and color.
-        sta zpw_coloraddr
-        bcc copy_chars          // If no overflow, no need to increment hi byte.
-        ldy zpw_screenaddr+1    // Load screen/color hi bytes, increment and write back.
-        iny
-        sty zpw_screenaddr+1
-        ldy zpw_coloraddr+1
-        iny
-        sty zpw_coloraddr+1
+        lda #<screen_base       // If base lo bytes for screen and color are not 00,
+                                // this will go wrong.
+        clc
+        adc zpw_cursoraddr      // Add cursor lo byte.
+        sta zpw_screenaddr      // Apply cursor movement to screen...
+        sta zpw_coloraddr       // ...and color RAM.
 
-copy_chars:
         ldy #$00                // Counter for indexing through characters.
 nextchar:
         lda (zpw_textaddr),Y    // Load current char into A.
@@ -223,12 +231,13 @@ nextchar:
         iny
         jmp nextchar
 endstring:
+        tya                     // Copy # of chars printed from Y to A.
         rts                     // Done, return.
 
         *=$1000 "Data"
 txtstart:
 txtmike:
-        .text "mikerofone"
+        .text "mikerofone "
         .byte 0
 txtmountain:
         .text "at mountainbytes"
